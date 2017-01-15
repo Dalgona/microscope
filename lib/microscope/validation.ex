cfd428f5f19f2c5e7e2dd2fe78ac07038a7331fb
Dalgona <dalgona@hontou.moe>
2017-01-15 10:47:18 +0900

    Add documentations for `Microscope.Validation`
    * Use guards to check if `port` is in range
    * Add a guard to check if `callbacks` is a list

--- lib/microscope/validation.ex
+++ lib/microscope/validation.ex
@@ -1,4 +1,16 @@
 defmodule Microscope.Validation do
+  @moduledoc """
+  Provides functions for validating arguments or options to Microscope.
+  """
+
+  @doc """
+  Checks if the `webroot` argument is valid.
+
+  Returns `:ok` if `webroot` is the path to an existing directory.
+
+  Raises `ArgumentError` if `webroot` is not a directory, or `webroot` is not
+  a string value.
+  """
   @spec validate_webroot(term) :: :ok | no_return
 
   def validate_webroot(webroot) when is_binary(webroot) do
@@ -13,20 +25,33 @@ defmodule Microscope.Validation do
     raise ArgumentError, "`webroot' expects a string, got #{inspect x}"
   end
 
+  @doc """
+  Checks if the `port` option is valid.
+
+  Returns `:ok` if `port` is a positive integer less than 65536.
+
+  Raises `ArgumentError` if `port` is not an integer, or the value is out of
+  the acceptable range.
+  """
   @spec validate_port(term) :: :ok | no_return
 
-  def validate_port(port) when is_integer(port) do
-    if port < 1 or port > 0xFFFF do
-      raise ArgumentError, "`port' value out of range"
-    else
-      :ok
-    end
-  end
+  def validate_port(port)
+    when is_integer(port) and port > 0 and port < 0x10000,
+    do: :ok
 
-  def validate_port(x) do
-    raise ArgumentError, "`port' expects an integer value, got #{inspect x}"
-  end
+  def validate_port(port) when is_integer(port),
+    do: raise ArgumentError, "`port' value out of range"
 
+  def validate_port(x),
+    do: raise ArgumentError, "`port' expects an integer, got #{inspect x}"
+
+  @doc """
+  Checks if the `base` option is valid.
+
+  Returns `:ok` if `base` is a string.
+
+  Raises `ArgumentError` otherwise.
+  """
   @spec validate_base(term) :: :ok | no_return
 
   def validate_base(base) when is_binary(base), do: :ok
@@ -34,11 +59,19 @@ defmodule Microscope.Validation do
   def validate_base(x),
     do: raise ArgumentError, "`base' expects a string, got #{inspect x}"
 
-  @spec validate_callbacks([module]) :: :ok | no_return
+  @doc """
+  Checks if the `callbacks` option is valid.
+
+  Returns `:ok` if `callbacks` is a list of atoms.
+
+  Raises `ArgumentError` if the list contains non-atom values, or `callbacks`
+  is not a list.
+  """
+  @spec validate_callbacks(term) :: :ok | no_return
 
   def validate_callbacks([]), do: :ok
 
-  def validate_callbacks(cb_mods) do
+  def validate_callbacks(cb_mods) when is_list(cb_mods) do
     case Enum.reject cb_mods, &is_atom/1 do
       [] -> :ok
       _ -> raise ArgumentError,
@@ -46,6 +79,16 @@ defmodule Microscope.Validation do
     end
   end
 
+  def validate_callbacks(x), do: raise ArgumentError,
+    "`callbacks` expects a list of modules, got #{inspect x}"
+
+  @doc """
+  Checks if the `index` option is valid.
+
+  Returns `:ok` if `index` is a boolean value.
+
+  Raises `ArgumentError` otherwise.
+  """
   @spec validate_index(term) :: :ok | no_return
 
   def validate_index(index) when is_boolean(index), do: :ok
