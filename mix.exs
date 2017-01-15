f948a261304d5387447e682843100afce56bb547
Dalgona <dalgona@hontou.moe>
2017-01-15 11:13:13 +0900

    Add tests for `Microscope.Validation` module
    + Update `dialyxir`

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Microscope.Mixfile do
   def project do
     [app: :microscope,
      version: "1.0.0-beta",
-     elixir: "~> 1.3",
+     elixir: "> 1.3.2",
      description: "A simple static web server written in Elixir",
      package: package(),
      build_embedded: Mix.env == :prod,
@@ -27,7 +27,7 @@ defmodule Microscope.Mixfile do
     [{:cowboy, "~> 1.0"},
      {:mime, "~> 1.0"},
      {:credo, "~> 0.5.3", only: [:dev]},
-     {:dialyxir, "~> 0.4", only: [:dev], runtime: false},
+     {:dialyxir, "~> 0.4.3", only: [:dev], runtime: false},
      {:ex_doc, "~> 0.14", only: [:dev]}]
   end
 end
