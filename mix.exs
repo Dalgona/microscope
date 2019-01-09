4cea449e70e2512bac109120358680e2389b4b56
Dalgona <dalgona@hontou.moe>
2019-01-09 21:23:38 +0900

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Microscope.Mixfile do
   def project do
     [app: :microscope,
      version: "1.0.0",
-     elixir: "> 1.3.2",
+     elixir: "~> 1.6",
      description: "A simple static web server written in Elixir",
      package: package(),
      build_embedded: Mix.env == :prod,
@@ -25,9 +25,9 @@ defmodule Microscope.Mixfile do
 
   defp deps do
     [{:cowboy, "~> 1.0"},
-     {:mime, "~> 1.0"},
-     {:credo, "~> 0.5.3", only: [:dev]},
-     {:dialyxir, "~> 0.4.3", only: [:dev], runtime: false},
-     {:ex_doc, "~> 0.14", only: [:dev]}]
+     {:mime, "~> 1.3"},
+     {:credo, "~> 1.0", only: [:dev]},
+     {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+     {:ex_doc, "0.18.4", only: [:dev]}]
   end
 end
