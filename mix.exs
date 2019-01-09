041a99a6f411ab789db3ee346af96af05b1e30a0
Dalgona <dalgona@hontou.moe>
2019-01-09 21:34:09 +0900

    Release 1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Microscope.Mixfile do
   def project do
     [
       app: :microscope,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.6",
       description: "A simple static web server written in Elixir",
       package: package(),
