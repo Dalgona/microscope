19aaa83c49019ed8489775814e716c7246100b40
Dalgona <me@dalgona.dev>
2020-12-27 11:11:46 +0900

    refactor: Microscope.Logger

--- lib/microscope/logger.ex
+++ lib/microscope/logger.ex
@@ -3,30 +3,28 @@ defmodule Microscope.Logger do
 
   @behaviour Microscope.Callback
 
-  @spec on_request() :: no_return
+  @spec on_request() :: :ok
+  def on_request, do: :ok
 
-  def on_request, do: nil
-
-  @spec on_200(String.t(), String.t(), String.t()) :: no_return
-
+  @spec on_200(String.t(), String.t(), String.t()) :: :ok
   def on_200(from, method, path), do: log(200, from, method, path)
 
-  @spec on_404(String.t(), String.t(), String.t()) :: no_return
-
+  @spec on_404(String.t(), String.t(), String.t()) :: :ok
   def on_404(from, method, path), do: log(404, from, method, path)
 
-  @spec log(non_neg_integer, String.t(), String.t(), String.t()) :: :ok
-
+  @spec log(non_neg_integer(), String.t(), String.t(), String.t()) :: :ok
   defp log(status, from, method, path) do
-    message =
-      "#{color_from_status(status)}[#{status}]\e[0m " <>
-        "#{from} #{method} " <> "\e[1m#{path}\e[0m"
-
-    IO.puts(message)
+    [
+      [color_from_status(status), "[#{status}]", :reset],
+      [" #{from} #{method} ", :bright, path]
+    ]
+    |> IO.ANSI.format()
+    |> IO.puts()
   end
 
-  @spec color_from_status(non_neg_integer) :: String.t()
-
-  defp color_from_status(200), do: "\e[32m"
-  defp color_from_status(404), do: "\e[31m"
+  @spec color_from_status(non_neg_integer()) :: IO.ANSI.ansicode()
+  defp color_from_status(status)
+  defp color_from_status(status) when status in 200..299, do: :green
+  defp color_from_status(status) when status in 400..499, do: :red
+  defp color_from_status(_status), do: :reset
 end
